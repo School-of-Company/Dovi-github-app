5cce25602a9a1c431e2e842889a67803750354bd
jyx-07 <s25069@gsm.hs.kr>
2026-06-17 18:07:07 +0900

    add :: AppModule 전역 모듈 와이어링

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebhookModule } from './webhook/webhook.module';
+import { RedisModule } from './redis/redis.module';
+import { KafkaModule } from './kafka/kafka.module';
+import { InstallationTokenModule } from './installation-token/installation-token.module';
+import { ReviewResultConsumerModule } from './review-result-consumer/review-result-consumer.module';
 
 @Module({
-  imports: [WebhookModule],
+  imports: [
+    RedisModule,
+    KafkaModule,
+    InstallationTokenModule,
+    WebhookModule,
+    ReviewResultConsumerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
